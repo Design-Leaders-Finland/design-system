4bc322a6105deb2ede1e33f3b0b73e682ea7a023
Hamppy <a.j.hamalainen@outlook.com>
2026-06-29 19:50:50 +0300

    Color themes created as a onepage web app

--- lib/design_system.dart
+++ lib/design_system.dart
@@ -7,3 +7,5 @@ export "src/utils/utils.dart";
 export "src/widgets/button/button.dart";
 export "src/widgets/text/text.dart";
 export "src/theme/theme.dart";
+export "src/theme/color_scheme.dart";
+export "src/theme/text_scheme.dart";
